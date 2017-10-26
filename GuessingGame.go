15aeaff91e0eb11243c9a379b08156a8a98026fe
smcguire56 <sean.mcguire56@gmail.com>
2017-10-26 22:55:05 +0100

    added template file

--- GuessingGame.go
+++ GuessingGame.go
@@ -1,27 +1,38 @@
 package main
 
 import (
-  	"net/http"
+	"html/template"
+	"net/http"
+	"strconv"
 )
 
-func printString(w http.ResponseWriter, r *http.Request){
-
-	w.Header().Set("Content-Type","text/html");// allows browser to render html tags
-	//fmt.Fprint(w, "<h1>Guessing Game</h1>")
-	http.ServeFile(w, r,"index.html")
+type msg struct {
+	Title string
+	Guess int
 }
-func guessGame(w http.ResponseWriter, r *http.Request){
 
-	w.Header().Set("Content-Type","text/html");// allows browser to render html tags
+func printString(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "text/html") // allows browser to render html tags
 	//fmt.Fprint(w, "<h1>Guessing Game</h1>")
-	http.ServeFile(w, r,"guess.html")
+	http.ServeFile(w, r, "index.html")
+}
+func guessGame(w http.ResponseWriter, r *http.Request) {
+
+	//w.Header().Set("Content-Type", "text/html") // allows browser to render html tags
+	//fmt.Fprint(w, "<h1>Guessing Game</h1>")
+	//http.ServeFile(w, r, "guess.html")
+
+	guess, _ := strconv.Atoi(r.FormValue("guess"))
+	messageStruct := &msg{Title: "Guess a number between 1 and 20", Guess: guess}
+
+	t, _ := template.ParseFiles("guess.tmpl")
+	t.Execute(w, messageStruct)
 }
 
 func main() {
+
 	http.HandleFunc("/", printString)
 	http.HandleFunc("/guess", guessGame)
-	
-	
-	
 	http.ListenAndServe(":8080", nil)
 }
