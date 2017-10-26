7d10f1033c33f1cb6203656c7d7e9bdf0fa1ef23
SEAN MC GUIRE - Student <G00330886@gmit.ie>
2017-10-26 19:49:42 +0100

    added new guess page

--- GuessingGame.go
+++ GuessingGame.go
@@ -10,8 +10,18 @@ func printString(w http.ResponseWriter, r *http.Request){
 	//fmt.Fprint(w, "<h1>Guessing Game</h1>")
 	http.ServeFile(w, r,"index.html")
 }
+func guessGame(w http.ResponseWriter, r *http.Request){
+
+	w.Header().Set("Content-Type","text/html");// allows browser to render html tags
+	//fmt.Fprint(w, "<h1>Guessing Game</h1>")
+	http.ServeFile(w, r,"guess.html")
+}
 
 func main() {
 	http.HandleFunc("/", printString)
+	http.HandleFunc("/guess", guessGame)
+	
+	
+	
 	http.ListenAndServe(":8080", nil)
 }
