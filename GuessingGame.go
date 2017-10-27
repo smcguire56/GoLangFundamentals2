d7a681741d05241229ac052937de589619b39ceb
SEAN MC GUIRE - Student <G00330886@gmit.ie>
2017-10-27 16:23:43 +0100

    added comments

--- GuessingGame.go
+++ GuessingGame.go
@@ -2,22 +2,24 @@ package main
 
 import (
 	"html/template"
+	"math/rand"
 	"net/http"
 	"strconv"
 	"time"
-	"math/rand"
 )
 
 // https://gobyexample.com/structs
 type msg struct {
-	Header string
-	Guess int
+	Header  string
+	Guess   int
 	Message string
 }
 
+// https://golang.org/pkg/net/http/ 
 func printString(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
+
 
 func guessGame(w http.ResponseWriter, r *http.Request) {
 	newRandomNum := getRandomNumber(1, 20)
@@ -25,26 +27,29 @@ func guessGame(w http.ResponseWriter, r *http.Request) {
 	// https://golang.org/src/net/http/cookie.go
 	cookie, err := r.Cookie("target")
 
-	if err != nil {	
-		cookie = &http.Cookie{ Name: "target",
-							   Value: strconv.Itoa(newRandomNum),				// https://golang.org/pkg/strconv/
-							   Expires: time.Now().Add(72 * time.Hour),
+	if err != nil {
+		cookie = &http.Cookie{Name: "target",
+			Value:   strconv.Itoa(newRandomNum), // https://golang.org/pkg/strconv/
+			Expires: time.Now().Add(72 * time.Hour),
 		}
 
-		http.SetCookie(w,cookie)
+		http.SetCookie(w, cookie)
 	}
 
+	// get the input from guess
 	guess, _ := strconv.Atoi(r.FormValue("guess"))
 	messageStruct := &msg{Header: "Guess a number between 1 and 20", Guess: guess}
 	target, _ := strconv.Atoi(cookie.Value)
 
+	// compare the values of the guess and the random number
+	// if they are equal print you got it  
 	if target == guess {
 		cookie = &http.Cookie{
-			Name: "target",
-			Value: strconv.Itoa(newRandomNum),
+			Name:    "target",
+			Value:   strconv.Itoa(newRandomNum),
 			Expires: time.Now().Add(365 * 24 * time.Hour),
 		}
-
+		// generate a new number and save as the cookie
 		http.SetCookie(w, cookie)
 
 		messageStruct.Message = "You Guessed Correctly!"
@@ -54,18 +59,15 @@ func guessGame(w http.ResponseWriter, r *http.Request) {
 		messageStruct.Message = "Too high, try again"
 	}
 
-
-
 	t, _ := template.ParseFiles("guess.tmpl")
 	t.Execute(w, messageStruct)
-
 }
-
+// generate random numbers
+// source http://golangcookbook.blogspot.ie/2012/11/generate-random-number-in-given-range.html
 func getRandomNumber(min, max int) int {
-    rand.Seed(time.Now().Unix())
-    return rand.Intn(max - min) + min
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max-min) + min
 }
-
 
 func main() {
 	http.HandleFunc("/", printString)
