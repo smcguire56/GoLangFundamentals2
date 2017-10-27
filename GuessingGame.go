2a2b6d1adf2b9e8da12d9e69eb37170deaefa4d0
SEAN MC GUIRE - Student <G00330886@gmit.ie>
2017-10-27 15:52:27 +0100

    post added

--- GuessingGame.go
+++ GuessingGame.go
@@ -10,47 +10,55 @@ import (
 
 // https://gobyexample.com/structs
 type msg struct {
-	Title string
+	Header string
 	Guess int
+	Message string
 }
 
 func printString(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("Content-Type", "text/html") // allows browser to render html tags
-	//fmt.Fprint(w, "<h1>Guessing Game</h1>")
 	http.ServeFile(w, r, "index.html")
 }
-func guessGame(w http.ResponseWriter, r *http.Request) {
-	myrand := getRandomNumber(1, 20)
 
-	// Try to read the cookie.
+func guessGame(w http.ResponseWriter, r *http.Request) {
+	newRandomNum := getRandomNumber(1, 20)
+
+	// https://golang.org/src/net/http/cookie.go
 	cookie, err := r.Cookie("target")
 
 	if err != nil {	
-		cookie = &http.Cookie{ Name: "target", Value: strconv.Itoa(myrand), Expires: time.Now().Add(72 * time.Hour),
+		cookie = &http.Cookie{ Name: "target",
+							   Value: strconv.Itoa(newRandomNum),				// https://golang.org/pkg/strconv/
+							   Expires: time.Now().Add(72 * time.Hour),
 		}
 
 		http.SetCookie(w,cookie)
 	}
 
 	guess, _ := strconv.Atoi(r.FormValue("guess"))
-	messageStruct := &msg{Title: "Guess a number between 1 and 20", Guess: guess}
-
+	messageStruct := &msg{Header: "Guess a number between 1 and 20", Guess: guess}
 	target, _ := strconv.Atoi(cookie.Value)
 
 	if target == guess {
-		// correct
 		cookie = &http.Cookie{
 			Name: "target",
-			Value: strconv.Itoa(myrand),
-			Expires: time.Now().Add(72 * time.Hour),
+			Value: strconv.Itoa(newRandomNum),
+			Expires: time.Now().Add(365 * 24 * time.Hour),
 		}
 
 		http.SetCookie(w, cookie)
+
+		messageStruct.Message = "You Guessed Correctly!"
+	} else if guess < target {
+		messageStruct.Message = "Too low, try again."
+	} else if guess > target {
+		messageStruct.Message = "Too high, try again"
 	}
+
+
 
 	t, _ := template.ParseFiles("guess.tmpl")
 	t.Execute(w, messageStruct)
+
 }
 
 func getRandomNumber(min, max int) int {
@@ -60,7 +68,6 @@ func getRandomNumber(min, max int) int {
 
 
 func main() {
-
 	http.HandleFunc("/", printString)
 	http.HandleFunc("/guess", guessGame)
 	http.ListenAndServe(":8080", nil)
